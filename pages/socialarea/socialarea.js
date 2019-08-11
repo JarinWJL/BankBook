71375ec65a2dc6a9af76b6ffad7a6f1399653bf8
ZhiHong_Xu <47109379+LearnerHong@users.noreply.github.com>
2019-08-11 19:13:31 +0800

    对社区页共读部分进行完善

--- pages/socialarea/socialarea.js
+++ pages/socialarea/socialarea.js
@@ -12,27 +12,26 @@ Page({
     winHeight: 0,
     // tab切换  
     currentTab: 0,  
-
+    score: 89,
+    currentIndex: 2,
+    count: 0
   },
-
   /**
-   * 生命周期函数--监听页面加载
-   */
+    * 加载页面时，渲染页面之前对页面对象进行初始化
+    */
   onLoad: function (options) {
-    var that = this; 
-/** 
-     * 获取系统信息 
-     */
-    wx.getSystemInfo({
-
-      success: function (res) {
+    var that = this;
+    const db = wx.cloud.database()
+    //API风格:回调风格
+    db.collection("CommenList").get({
+      //执行成功必须要有
+      success: res => {
+        console.log("获取共读书单列表：", res.data);
         that.setData({
-          winWidth: res.windowWidth,
-          winHeight: res.windowHeight
-        });
-      }
-
-    });  
+          bookCommenlist: res.data,
+        })
+      },
+    })
   },
   /** 
       * 滑动切换tab 
@@ -44,7 +43,7 @@ Page({
 
   },
   /** 
-   * 点击tab切换 
+   * 点击tab切换:dataset为页面自定义属性，通过tap实现数据的传递
    */
   swichNav: function (e) {
 
@@ -52,59 +51,18 @@ Page({
 
     if (this.data.currentTab === e.target.dataset.current) {
       return false;
-    } else {
+    }
+     else if (e.target.dataset.current == null)
+     {
+       console.log("没有捕捉到目标值")
+     }
+     else {
       that.setData({
         currentTab: e.target.dataset.current
       })
+      console.log("目标值是：" + e.target.dataset.current)
     }
-  }  ,
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  } 
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
   
 })
